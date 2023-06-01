389148f68ef600f573f540857a95c1c2426384d9
MissMozart <130683435+MissMozart@users.noreply.github.com>
2023-06-01 11:10:10 -0400

    Added sound effect

---
+++ src/main/java/org/yearup/sandwichshop/PlaySound.java
@@ -0,0 +1,48 @@
+package org.yearup.sandwichshop;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.SourceDataLine;
+import java.io.File;
+import java.io.IOException;
+
+public class PlaySound {
+    private File file;
+    private AudioInputStream audio;
+    private byte[] buffer;
+    private SourceDataLine line;
+
+        public PlaySound(String fileName) {
+            try {
+                // Load WAV file into byte array
+                file = new File("./Sound/bell-ringing-04.wav");
+                audio = AudioSystem.getAudioInputStream(file);
+                buffer = new byte[(int) audio.getFrameLength() * audio.getFormat().getFrameSize()];
+                audio.read(buffer);
+
+                // Create SourceDataLine object for playing the sound
+                line = AudioSystem.getSourceDataLine(audio.getFormat());
+                line.open();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void playSound() {
+            try {
+                // Start playing the sound
+                line.start();
+                line.write(buffer, 0, buffer.length);
+
+                // Wait for the sound to finish playing
+                line.drain();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                // Close the line
+                line.close();
+            }
+        }
+    }
+
